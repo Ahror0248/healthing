cc389fb60309668430ce3c7cf9f796004088a1b3
Acer-Samsung <84494005+Acer-Samsung@users.noreply.github.com>
2021-09-15 16:31:28 +0500

    rbjhvhsdc

--- src/Components/Database.js
+++ src/Components/Database.js
@@ -64,7 +64,7 @@ export const Database = [
             "beef, tomatoes,\n" +
             "mozzarella and chipotle sauce\n",
         price: "23,000",
-        imgSrc: "../assets/img/Cheeseburger.png"
+        imgSrc: "../assets/img/Bitmapche.png"
     }
 
 ]
